e3ab12d220d9863f2ffbefb224a1e7665a481cb2
Vinhdev <vinhluong1722@gmail.com>
2024-05-05 13:59:55 +0700

    update frontend

---
+++ frontend/bookshop/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import Navigation from './components/Navigation/Navigation'
+
+function App() {
+  return (
+    <div className="">
+      <Navigation />
+      <div>Home</div>
+    </div>
+  )
+}
+
+export default App
