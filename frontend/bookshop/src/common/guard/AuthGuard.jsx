6bd67a3b27d478f6213423e8b6d2c4b0e154a20c
ndtien142 <ndtien142@gmail.com>
2024-06-08 06:40:05 +0700

    check role

--- frontend/bookshop/src/common/guard/AuthGuard.jsx
+++ frontend/bookshop/src/common/guard/AuthGuard.jsx
@@ -1,12 +1,20 @@
 // src/components/AuthGuard.js
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { logout } from "../../State/Auth/Action";
 
 const AuthGuard = ({ children }) => {
-  const { jwt } = useSelector((state) => state.auth);
+  const { user, jwt } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-  if (!jwt) {
+  useEffect(() => {
+    if (jwt && (!user || user.role !== "admin")) {
+      dispatch(logout());
+    }
+  }, [user, jwt, dispatch]);
+
+  if (!jwt || (user && user.role !== "admin")) {
     return <Navigate to="/login" />;
   }
 
