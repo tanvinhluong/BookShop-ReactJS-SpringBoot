0993fe315e09ba844ee3037a96076e47e92ae9d4
Vinhdev <vinhluong1722@gmail.com>
2024-06-08 19:32:18 +0700

    update

--- frontend/bookshop/src/customer/pages/HomePage/HomePage.jsx
+++ frontend/bookshop/src/customer/pages/HomePage/HomePage.jsx
@@ -106,7 +106,7 @@ const HomePage = () => {
           backgroundColor: 'pink',
         }}
       >
-        <div>Chọn Danh Mục 2: </div>
+        <div>Chọn Danh Mục: </div>
         <select value={category[selectedIndex]} onChange={handleComboBoxChange}>
           {/* Đổ dữ liệu từ mảng vào combo box */}
           {category.map((option, index) => (
